eb94e5488ffb35caddff07c6473ff99324836fec
League Student <student@jointheleague.org>
2026-07-02 12:56:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/20_Turtle_Tricks.py
@@ -13,7 +13,8 @@ Refer to the previous program, Meet_Tina.py, for examples of how to use turtle c
 import turtle            # Tell Python we want to work with the turtle
 turtle.setup(600, 600, 0, 0)            # Set the size of the window
 tina = turtle.Turtle()
-window = turtke.Screen('black')                  # Create a turtle named tina
+window = turtle.Screen()
+window.bgcolor('black')                  # Create a turtle named tina
 
 tina.forward(80)
 tina.left(72)
