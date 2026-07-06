bb3db96dd2cffbc8cf6f9358e07f46feb1c73c5d
League Student <student@jointheleague.org>
2026-07-06 13:37:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
@@ -12,3 +12,21 @@ Use this code to get a random x and y location
     y = random.randint(-300, 300)
 """
 
+import turtle                               # Import the turtle module
+
+screen = turtle.Screen()                # Set up the screen
+screen.setup(width=600, height=600)     # Set the size of the window
+screen.bgcolor('white')                 # Set the background color
+
+t1 = turtle.Turtle()                    # Create the first turtle
+t1.penup()                              # Lift the pen to move without drawing
+t1.shape("turtle")                      # Set the shape of the turtle
+
+t2 = turtle.Turtle()                    # Create the second turtle
+t2.penup()                              # Lift the pen to move without drawing
+t2.shape("arrow")                       # Set the shape of the turtle
+
+# Move both turtles in a loop
+for i in range(-200, 200):
+    t1.goto(i, i)
+    t2.goto(i, -i)
