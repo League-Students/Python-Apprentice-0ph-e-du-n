5ea88fadcee7d9f6eebe4406215d4311b7afbd09
League Student <student@jointheleague.org>
2026-07-10 13:19:01 -0700

    Auto-commit: workspace changes before push

--- lessons/50_Projects/20_Random_Walk.py
+++ lessons/50_Projects/20_Random_Walk.py
@@ -13,7 +13,9 @@ cam_colors = ('red', 'gray', 'white', 'blue', 'green')
 
 def move_tina():
     global tina_progress
-    tina_progress +=
+    tina_progress += 1
+    show
+    screen.ontimer(move_tina 2000)
 
 def show_animatronics(cam_num):
     #tina show
