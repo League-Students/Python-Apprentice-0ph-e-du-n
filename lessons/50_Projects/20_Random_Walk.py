91d089c755ebd832079e3c7af9b2967cdaf37413
League Student <student@jointheleague.org>
2026-07-10 12:47:01 -0700

    Auto-commit: workspace changes before push

--- lessons/50_Projects/20_Random_Walk.py
+++ lessons/50_Projects/20_Random_Walk.py
@@ -9,7 +9,23 @@ cam_colors = ['red', 'black, 'white', 'blue','green']
 
 def open_cam1():
     print ("cam 5 open")
-    screen. bgcolor(cam_colors[0])
+    screen. bgcolor(cam_colors[0]
+
+def open_cam1():
+    print ("cam 5 open")
+    screen. bgcolor(cam_colors[0]
+
+def open_cam1():
+    print ("cam 5 open")
+    screen. bgcolor(cam_colors[0]
+
+def open_cam1():
+    print ("cam 5 open")
+    screen. bgcolor(cam_colors[0]
+
+def open_cam1():
+    print ("cam 5 open")
+    screen. bgcolor(cam_colors[0]
 
 # Close the turtle window on click
 screen.exitonclick()
