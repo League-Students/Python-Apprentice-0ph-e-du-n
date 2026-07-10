2a0becd29d6ce953451152cc424cf6094121a666
League Student <student@jointheleague.org>
2026-07-10 13:01:01 -0700

    Auto-commit: workspace changes before push

--- lessons/50_Projects/20_Random_Walk.py
+++ lessons/50_Projects/20_Random_Walk.py
@@ -11,6 +11,10 @@ screen.setup(500,500)
 
 cam_colors = ('red', 'black', 'white', 'blue', 'green')
 
+def show_animatronics(cam_num):
+    
+
+
 def open_cam1():
     print ("cam 1 open")
     screen. bgcolor(cam_colors[0])
